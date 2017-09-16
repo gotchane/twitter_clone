a0382fd7570da567430e6aa8996dddcdfd11b080
5010m <m.goto.jb@gmail.com>
2017-09-16 11:44:38 +0900

    add relationship model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,34 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #
 #
+# ユーザー 
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+99.times do |n|
+  name  = "TestUser" + n.to_s
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
+
+# マイクロポスト 
 users = User.order(:created_at).take(6)
-10.times do
-  content = "hogehogehoge"
+50.times do |n|
+  content = "This is a test tweet no #{n}"
   users.each { |user| user.tweets.create!(tweet_text: content) }
 end
+
+# リレーションシップ 
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+
