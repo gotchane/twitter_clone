e092dd392b792be94fafed99bb8379676e82d39f
5010m <m.goto.jb@gmail.com>
2017-09-18 19:52:43 +0900

    add specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash.now[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash.now[:success] = "Welcome to the Twitter clone!"
       redirect_to @user
     else
       render 'new'
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-
   describe "GET #new" do
+    render_views
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
     end
+    it "render template successfully" do
+      get :new
+      expect(response.body).to include('Email')
+      expect(response.body).to include('Password')
+      expect(response.body).to include('Log in')
+      expect(response.body).to include('Sign up')
+      expect(response.body).to include('Back')
+    end
   end
-
 end
