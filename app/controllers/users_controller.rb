f333f3511833a8137d6d5aa6dea124613b6bdb41
5010m <m.goto.jb@gmail.com>
2017-09-18 20:06:42 +0900

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -20,6 +21,10 @@ class UsersController < ApplicationController
     @tweet_post = @user.tweets.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def update
     @user = User.find(params[:id])
   end
@@ -29,4 +34,10 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
