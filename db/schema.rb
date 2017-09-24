39e99d776d2baa13feedcbc1276d9a86a08024f6
Masayuki Goto <originals0711@gmail.com>
2017-09-24 16:41:31 +0900

    fix view

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170924070638) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.index ["user_id", "created_at"], name: "index_tweets_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
