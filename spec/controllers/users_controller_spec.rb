0a28fa8ed41e135288c5b4d41b94db47af79fd44
Masayuki Goto <originals0711@gmail.com>
2017-09-24 12:34:58 +0900

    edit users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe 'GET #index' do
+    render_views
+    it "links successfully" do
+      expect(get: users_path).to route_to(controller: "users",action: "index")
+      expect(response.status).to eq(200)
+    end
+    it "render template successfully" do
+      get :index
+    end
+  end
+
   describe 'GET #new' do
     render_views
     it "links successfully" do
