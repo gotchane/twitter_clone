e358fc7cc2cff237126b640f078103c7ba5ff5dd
Masayuki Goto <originals0711@gmail.com>
2017-10-01 14:23:49 +0900

    update task

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,19 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, 
+  validates :email, presence: true, uniqueness: true,
             length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: { minimum: 6 }
   validates :profile, length: {maximum: 100}
   has_secure_password
   has_many :tweets
-  has_many :active_relationships, class_name:  "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent:   :destroy
-  has_many :passive_relationships, class_name: "Relationship",
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   mount_uploader :avater, AvaterUploader
 
@@ -29,5 +29,4 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
-
 end
