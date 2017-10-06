a263c3982c78abfeef19537e40e1f46c6ccb72b5
5010m <m.goto.jb@gmail.com>
2017-10-06 10:31:15 +0900

    CarrerWaveの画像アップロード先を本番環境のみAWSに修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,17 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: Rails.application.secrets.aws_region,
-    path_style: true
-  }
-  config.fog_public = true
-  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
-  config.remove_previously_stored_files_after_update = false
-  config.fog_directory = Rails.application.secrets.aws_s3_bucket
+  if Rails.env.production?
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: Rails.application.secrets.aws_region,
+      path_style: true
+    }
+    config.fog_public = true
+    config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+    config.remove_previously_stored_files_after_update = false
+    config.fog_directory = Rails.application.secrets.aws_s3_bucket
+  else
+    config.storage = :file
+  end
 end
