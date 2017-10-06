3eaab5604a3b0543b59c069b89f322e580261c4d
5010m <m.goto.jb@gmail.com>
2017-10-06 10:41:50 +0900

    微修正

--- db/migrate/20170903064816_create_tweets.rb
+++ db/migrate/20170903064816_create_tweets.rb
@@ -4,7 +4,6 @@ class CreateTweets < ActiveRecord::Migration[5.1]
       t.string :tweet_text, limit: 140
       t.string :image
       t.references :user, foreign_key: true
-
       t.timestamps
     end
     add_index :tweets, [:user_id, :created_at]
