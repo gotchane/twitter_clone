80f6b02388e0d0eb13d1d6abf4e41fb99a2d8471
5010m <m.goto.jb@gmail.com>
2017-10-07 16:27:11 +0900

    helperの認証系処理をcontrollerに移管

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,28 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
   before_action :logged_in_user
-  include SessionsHelper
-  include UsersHelper
+  helper_method :current_user, :logged_in?
 
   def logged_in_user
     unless logged_in?
       redirect_to login_url, danger: "Please log in."
     end
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 end
