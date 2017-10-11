c110ebd21889273d77b7d3a19875df108172f9a1
Masayuki Goto <originals0711@gmail.com>
2017-10-12 06:41:47 +0900

    ログイン後のrootへのリダイレクト追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,12 @@
 class StaticPagesController < ApplicationController
   skip_before_action :logged_in_user, only: [:home]
+  before_action :root_after_login, only: [:home]
 
   def home
   end
+
+  private
+    def root_after_login
+      redirect_to current_user if logged_in?
+    end
 end
