b578d1de501028ab9c11b23ebfbf2189c92b6c8c
5010m <m.goto.jb@gmail.com>
2017-10-13 07:08:10 +0900

    migrationファイルをrevert

--- db/migrate/20170903064449_create_users.rb
+++ db/migrate/20170903064449_create_users.rb
@@ -3,11 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :email, null: false
-      t.string :password_digest
-      t.string :avater
-      t.text   :profile
       t.timestamps
     end
-    add_index :users, :email, unique: true
   end
 end
