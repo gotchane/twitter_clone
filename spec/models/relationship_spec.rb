4677a61cae27157c68e663b7e1006177addd867d
Masayuki Goto <originals0711@gmail.com>
2017-10-16 07:04:23 +0900

    spec修正

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -7,16 +7,4 @@ RSpec.describe Relationship, type: :model do
     it { should belong_to(:follower) }
     it { should belong_to(:followed) }
   end
-  describe 'valid' do
-    it "can follow and unfollow" do
-      followed_user = user
-      followed_user.email = 'followed@example.com'
-      user.save!
-      followed_user.save!
-      user.follow(followed_user)
-      expect(user.following?(followed_user)).to be true
-      user.unfollow(followed_user)
-      expect(user.following?(followed_user)).to be false
-    end
-  end
 end
