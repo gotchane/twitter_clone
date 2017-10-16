28f9967cc20265698fb0821461b45f22c2be5a62
Masayuki Goto <originals0711@gmail.com>
2017-10-17 07:28:20 +0900

    Userのrspec,FactoryGirl修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,34 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    trait :name_none do
-      name nil
-      email 'example@example.com'
-      password 'examplepassword'
-      password_confirmation 'examplepassword'
-    end
-    trait :name_example do
-      name "example user"
-      email 'example@example.com'
-      password 'examplepassword'
-      password_confirmation 'examplepassword'
-    end
-    trait :over_max_email do
-      name "example user"
-      email 'a' * 255 + '@example.com'
-      password 'examplepassword'
-      password_confirmation 'examplepassword'
-    end
-    trait :under_min_password do
-      name "example user"
-      email 'example@example.com'
-      password 'a' * 5
-      password_confirmation 'a' * 5
-    end
-    trait :password_blank do
-      name "example user"
-      email 'example@example.com'
-      password nil
-      password_confirmation nil
-    end
+    name "example user"
+    email 'example@example.com'
+    password 'examplepassword'
+    password_confirmation 'examplepassword'
   end
 end
