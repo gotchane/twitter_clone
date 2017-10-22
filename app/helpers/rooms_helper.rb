19bca05065321b7d843f6ab61dca458d9508f6a5
Masayuki Goto <originals0711@gmail.com>
2017-10-22 20:21:25 +0900

    既読表示をhelperに分離

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -2,4 +2,19 @@ module RoomsHelper
   def room_participants(user, i)
     i == 0 ? user.name : "/ " + user.name
   end
+
+  def read_user_count(users, room, message)
+    read_count = 0
+    latest_read_message_id = current_user.user_rooms
+                                         .find_by(room_id: room.id)
+                                         .latest_read_message_id
+    users.each do |user|
+      unless user == current_user || latest_read_message_id.nil? then
+        if message.id < latest_read_message_id then
+          read_count += 1
+        end
+      end
+    end
+    read_count == 0 ? "Unread" : "Read:#{read_count}"
+  end
 end
