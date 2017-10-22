9b54e98e9647b374b5ddc65386cd9fb6ad4fdff4
Masayuki Goto <originals0711@gmail.com>
2017-10-22 23:31:23 +0900

    既読管理表示修正

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -4,18 +4,24 @@ module RoomsHelper
   end
 
   def read_user_count(users, room, message)
-    read_count = 0
-    latest_read_message_id = current_user.user_rooms
-                                         .find_by(room_id: room.id)
-                                         .latest_read_message_id
-    users.each do |user|
-      unless user == current_user || latest_read_message_id.nil? then
-        if message.id < latest_read_message_id then
-          read_count += 1
+    if message.user == current_user then
+      read_count = 0
+      users.each do |user|
+        latest_read_message_id = user.user_rooms
+                                     .find_by(room_id: room.id)
+                                     .latest_read_message_id
+        if user != current_user && !latest_read_message_id.nil? then
+          if message.id <= latest_read_message_id then
+            read_count += 1
+          end
         end
       end
+      if read_count == 0 then
+        "Unread"
+      else
+        "Read:#{read_count}"
+      end
     end
-    read_count == 0 ? "Unread" : "Read:#{read_count}"
   end
 
   def read_state(room)
