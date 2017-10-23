1af284c114887120519729a31a48e9eae80a5adf
Masayuki Goto <originals0711@gmail.com>
2017-10-23 15:24:07 +0900

    Pagination追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,7 +7,7 @@ class RoomsController < ApplicationController
     @room = current_user.rooms.check_available.find(params[:id])
     @user_room = @room.user_rooms.find_by(user: current_user)
     @messages = @room.messages.after_history_deletion(@user_room.datetime_last_history_deleted)
-                              .order(id: "ASC").page(params[:page])
+                              .order(created_at: "DESC").page(params[:page])
     @message_post = current_user.messages.new
     UserRoom.update_latest_read_message(@room,current_user)
   end
