1114906e624781700e5b4e6aae6ed7c809a0c4e8
Masayuki Goto <originals0711@gmail.com>
2017-10-25 21:35:44 +0900

    spec追加

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -52,5 +52,36 @@ RSpec.describe RoomsController, type: :controller do
       }
       expect(response).to redirect_to user_room_path(user, Room.find_by(create_user_id: user.id))
     end
+    it "redirect to the :new template when creating dup room" do
+      user.rooms << create(:room, create_user_id: user.id)
+      user_room_invitee = create(:user_room, user: invitee, room: user.rooms.first)
+      post :create,
+      params: {
+        user_id: user.id,
+        room: {user_room: {user_id: [invitee.id] }}
+      }
+      expect(response).to redirect_to new_user_room_path(user)
+    end
+  end
+  describe 'GET #show' do
+    it "populates a room of current user" do
+      user.rooms << create(:room, create_user_id: user.id)
+      get :show, params: { user_id: user.id, id: user.rooms.first.id }
+      expect(assigns(:room)).to eq(user.rooms.first)
+    end
+    it "populates relations between users and a room" do
+    end
+    it "populates messages of room" do
+    end
+    it "assigns new Room to @room" do
+      user.rooms << create(:room, create_user_id: user.id)
+      get :show, params: { user_id: user.id, id: user.rooms.first.id }
+      expect(assigns(:message_post)).to be_a_new(Message)
+    end
+    it "renders the :show template" do
+      user.rooms << create(:room, create_user_id: user.id)
+      get :show, params: { user_id: user.id, id: user.rooms.first.id }
+      expect(response).to render_template :show
+    end
   end
 end
