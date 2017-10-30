1ffc5e9198ca7004600fc07c14068ba9b17beb58
Masayuki Goto <originals0711@gmail.com>
2017-10-30 10:47:48 +0900

    ControllerのSpec修正

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -6,15 +6,19 @@ RSpec.describe RoomsController, type: :controller do
   before { session[:user_id] = user.id }
   describe 'GET #index' do
     it "populates an array of all rooms related to current user" do
-      user.rooms << create_list(:room, 2, create_user_id: user.id)
+      room = create(:room, create_user_id: user.id,
+                           current_user: user,
+                           user_rooms_attributes:[{ user_id: user.id },{ user_id: invitee.id }] )
       get :index, params: { user_id: user.id }
       expect(assigns(:rooms)).to match_array(user.rooms)
     end
     it "populates an array of rooms without unavailable" do
-      user.rooms << create_list(:room, 2, create_user_id: user.id)
+      room = create(:room, create_user_id: user.id,
+                           current_user: user,
+                           user_rooms_attributes:[{ user_id: user.id },{ user_id: invitee.id }] )
       user.rooms.first.user_rooms.first.available_flag = false
       get :index, params: { user_id: user.id }
-      expect(assigns(:rooms).count).to eq(2)
+      expect(assigns(:rooms).count).to eq(1)
     end
     it "renders the :index template" do
       get :index, params: { user_id: user.id }
@@ -68,8 +72,9 @@ RSpec.describe RoomsController, type: :controller do
       expect(response).to redirect_to user_room_path(user, Room.find_by(create_user_id: user.id))
     end
     it "redirect to the :new template when creating dup room" do
-      user.rooms << create(:room, create_user_id: user.id)
-      user_room_invitee = create(:user_room, user: invitee, room: user.rooms.first)
+      room = create(:room, create_user_id: user.id,
+                           current_user: user,
+                           user_rooms_attributes:[{ user_id: user.id },{ user_id: invitee.id }] )
       post :create,
       params: {
         user_id: user.id,
@@ -78,7 +83,7 @@ RSpec.describe RoomsController, type: :controller do
           user_ids: [invitee.id]
         }
       }
-      expect(response).to redirect_to new_user_room_path(user)
+      expect(response).to render_template :new
     end
     it "redirect to the :new template when user_room_params is nil" do
       post :create,
@@ -88,52 +93,72 @@ RSpec.describe RoomsController, type: :controller do
           create_user_id: user.id
         }
       }
-      expect(response).to redirect_to new_user_room_path(user)
+      expect(response).to render_template :new
     end
   end
   describe 'GET #show' do
     it "populates a room of current user" do
-      user.rooms << create(:room, create_user_id: user.id)
+      room = create(:room, create_user_id: user.id,
+                           current_user: user,
+                           user_rooms_attributes:[{ user_id: user.id },{ user_id: invitee.id }] )
       get :show, params: { user_id: user.id, id: user.rooms.first.id }
       expect(assigns(:room)).to eq(user.rooms.first)
     end
     it "populates relations between users and a room" do
-      user.rooms << create(:room, create_user_id: user.id)
+      room = create(:room, create_user_id: user.id,
+                           current_user: user,
+                           user_rooms_attributes:[{ user_id: user.id },{ user_id: invitee.id }] )
       get :show, params: { user_id: user.id, id: user.rooms.first.id }
       expect(assigns(:user_room)).to eq(user.rooms.first.user_rooms.first)
     end
     it "populates messages of room" do
-      user.rooms << create(:room, create_user_id: user.id)
-      msg = create(:message, room: user.rooms.first, user: user, body:"test")
+      room = create(:room, create_user_id: user.id,
+                           current_user: user,
+                           user_rooms_attributes:[{ user_id: user.id },{ user_id: invitee.id }] )
+      msg = create(:message, room: user.rooms.first, user: user, current_user: user, body:"test")
       get :show, params: { user_id: user.id, id: user.rooms.first.id }
       expect(assigns(:messages)).to match_array([msg])
     end
     it "assigns new Room to @room" do
-      user.rooms << create(:room, create_user_id: user.id)
+      room = create(:room, create_user_id: user.id,
+                           current_user: user,
+                           user_rooms_attributes:[{ user_id: user.id },{ user_id: invitee.id }] )
       get :show, params: { user_id: user.id, id: user.rooms.first.id }
       expect(assigns(:message_post)).to be_a_new(Message)
     end
     it "renders the :show template" do
-      user.rooms << create(:room, create_user_id: user.id)
+      room = create(:room, create_user_id: user.id,
+                           current_user: user,
+                           user_rooms_attributes:[{ user_id: user.id },{ user_id: invitee.id }] )
       get :show, params: { user_id: user.id, id: user.rooms.first.id }
       expect(response).to render_template :show
     end
   end
   describe 'GET #mark_read' do
     it "populates a room of current user" do
-      user.rooms << create(:room, create_user_id: user.id)
+      room = create(:room, create_user_id: user.id,
+                           current_user: user,
+                           user_rooms_attributes:[{ user_id: user.id },{ user_id: invitee.id }] )
       get :show, params: { user_id: user.id, id: user.rooms.first.id }
       expect(assigns(:room)).to eq(user.rooms.first)
     end
     it "populates a room of current user" do
-      user.rooms << create(:room, create_user_id: user.id)
+      room = create(:room, create_user_id: user.id,
+                           current_user: user,
+                           user_rooms_attributes:[{ user_id: user.id },{ user_id: invitee.id }] )
       user_room = UserRoom.where("user_id = ? and room_id = ?", user.id, user.rooms.first.id)
-      user.rooms.first.messages << create_list(:message, 2, room: user.rooms.first, user: user, body:"test")
+      user.rooms.first.messages << create_list(:message, 2, room: user.rooms.first,
+                                                            user: user,
+                                                            current_user: user,
+                                                            body:"test")
       get :mark_read, params: { user_id: user.id, id: user.rooms.first.id }
       expect(user_room.first.latest_read_message_id).to eq(user.rooms.first.messages.last.id)
     end
     it "renders json" do
-      user.rooms << create(:room, create_user_id: user.id)
+      room = create(:room, create_user_id: user.id,
+                           current_user: user,
+                           user_rooms_attributes:[{ user_id: user.id },{ user_id: invitee.id }] )
+      msg = create(:message, room: user.rooms.first, user: user, current_user: user, body:"test")
       get :mark_read, params: { user_id: user.id, id: user.rooms.first.id }
       expect(response.status).to eq(200)
     end
