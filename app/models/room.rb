4dc7b63a8a12add5e8a831f69c598404a78a40c1
Masayuki Goto <originals0711@gmail.com>
2017-11-10 21:24:59 +0900

    has_unread_message?の不要なnil判定削除

--- app/models/room.rb
+++ app/models/room.rb
@@ -64,7 +64,7 @@ class Room < ApplicationRecord
   end
 
   def has_unread_message?(user)
-    room_latest_message_id = self.messages.order(id: :desc).first.id unless self.messages.count == 0
+    room_latest_message_id = self.messages.order(id: :desc).first.id
     latest_read_message_id = user.user_rooms.find_by(room: self).latest_read_message_id
     if self.messages.count != 0 && room_latest_message_id.to_i > latest_read_message_id.to_i
       true
